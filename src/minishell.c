a3f00d9949b7872abbc7ecf2e2ac93b8ae5fe98a
BeAzarym <calvinchabeau01@gmail.com>
2023-06-30 16:22:34 +0200

    base token before spliting

--- src/minishell.c
+++ src/minishell.c
@@ -6,7 +6,7 @@
 /*   By: cchabeau <cchabeau@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/25 17:34:10 by cchabeau          #+#    #+#             */
-/*   Updated: 2023/06/26 14:40:01 by cchabeau         ###   ########.fr       */
+/*   Updated: 2023/06/30 16:20:55 by cchabeau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,24 +17,26 @@ int	main(int argc, char **argv, char **envp)
 	(void)argv;
 	(void)argc;
 	(void)envp;
-	// t_token *lst = NULL;
+	t_tkn_stack *stack = NULL;
 	while (1)
 	{
 		char *res = readline("[Minishell] > ");
 		char **tab = arg_split(res, " \n\t\r\f\t\b");
 		ft_array_print(tab);
-		// int i = 0;
-		// while (tab[i])
-		// {
-		// 	printf("[TOKEN] %s\n", tab[i]);
-		// 	lst = create_token(tab[i], 0, lst);
-		// 	if (!lst)
-		// 		return (42);
-		// 	i++;
-		// }
-		// print_chained(lst);
+		stack = init_stack();
+		if (!stack)
+			return (7);
+		int i = 0;
+		while (tab[i])
+		{
+			stack = add_stack(tab[i], stack);
+			if (!stack)
+				return (42);
+			i++;
+		}
+		print_chained(stack->head);
 		ft_array_clear(tab);
-		// clear_lst(lst);
+		clear_lst(stack->head);
 	}
 	return (0);
 }
