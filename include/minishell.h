2081af97a1f4af8482dee2a08abb1d132e87ffdd
BeAzarym <calvinchabeau01@gmail.com>
2023-08-07 10:13:44 +0200

    env update

--- include/minishell.h
+++ include/minishell.h
@@ -13,12 +13,12 @@ enum				e_redirect
 	APPND = 3
 };
 
-typedef struct s_token_stack
+typedef struct s_token_lst
 {
 	struct s_token	*head;
 	struct s_token	*tail;
 	int				size;
-}					t_tkn_stack;
+}					t_tkn_lst;
 
 typedef struct s_token
 {
@@ -26,6 +26,20 @@ typedef struct s_token
 	char			*value;
 	struct s_token	*next;
 }					t_token;
+
+typedef struct s_env_lst
+{
+	struct s_env	*head;
+	struct s_env	*tail;
+	int				size;
+}					t_env_lst;
+
+typedef struct s_env
+{
+	char			*key;
+	char			*value;
+	struct s_env	*next;
+}					t_env;
 
 typedef struct s_cmd
 {
@@ -36,19 +50,25 @@ typedef struct s_cmd
 
 char				**arg_split(char *s, char *sep);
 t_token				*create_token(char *word, char type, t_token *lst);
-void				print_chained(t_token *lst);
+void				print_token(t_token *lst);
 t_token				*get_lstlast(t_token *lst);
 void				add_back(t_token *src, t_token *new);
 void				clear_lst(t_token *lst);
 t_token				*init_token(char *value);
-t_tkn_stack			*init_stack(void);
-t_tkn_stack	*add_stack(char *value,
-						t_tkn_stack *stack);
+t_tkn_lst			*init_tkn_lst(void);
+t_tkn_lst	*add_lst_tkn(char *value,
+						t_tkn_lst *lst);
 int					jump_to_next_sep(char *s, char *sep, int i);
 int					escape_quotes(char *str, int n);
 char				define_type(char *value);
 int					is_sep(char c, char *sep);
 int					locate_token(char *value, char *token);
-t_tkn_stack *token_split(t_tkn_stack *stack, t_tkn_stack *new);
+t_tkn_lst			*token_split(t_tkn_lst *stack, t_tkn_lst *new);
+t_env_lst			*init_env_lst(void);
+t_env_lst			*add_lst_env(char *key, char *value, t_env_lst *lst);
+t_env_lst			*init_envp(char **envp);
+char				**envp_split(char *str);
+void				print_env(t_env_lst *lst);
+void				clear_env_lst(t_env *lst);
 
 #endif
