e569a3670498b073e4088e625164b4236f0e955d
angassin <AnaisTry@users.noreply.github.com>
2023-08-09 14:52:57 +0200

    outfile ok, pb with infile

--- minishell/src/main.c
+++ minishell/src/main.c
@@ -6,22 +6,22 @@
 /*   By: angassin <angassin@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/14 11:10:33 by angassin          #+#    #+#             */
-/*   Updated: 2023/08/09 13:59:53 by angassin         ###   ########.fr       */
+/*   Updated: 2023/08/09 14:50:57 by angassin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-int	main(int argc, char **argv, char	**envp)
+int	main(int argc, char **argv, char **envp)
 {
 	int		status;
 	t_cmd	cmd;
 
 	(void)argc;
 	cmd.type_in = STDIN_OUT;
-	cmd.type_out = STDIN_OUT;
+	cmd.type_out = TRUNCATE;
 	cmd.infile = NULL;
-	cmd.outfile = NULL;
+	cmd.outfile = "out";
 	cmd.cmd = &argv[1];
 	cmd.next = NULL;
 
