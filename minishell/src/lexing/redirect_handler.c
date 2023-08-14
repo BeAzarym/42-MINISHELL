cc718ecf6d37b46e0f451231178bff6e4fb4ff21
Anais Gassin <angassin@student.s19.be>
2023-08-14 13:09:06 +0200

    restructured Calvin's part

---
+++ minishell/src/lexing/redirect_handler.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   redirect_handler.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: angassin <angassin@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/13 16:55:06 by cchabeau          #+#    #+#             */
+/*   Updated: 2023/08/14 13:03:28 by angassin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/minishell.h"
+
+// void	have_redirect_arg(t_tkn_lst *lst, t_cmd *cmd)
+// {
+// 	while (lst->head)
+// 	{
+// 		if (lst->head->next || (lst->head->type == 'I' || lst->head->type == 'O'))
+// 		{
+// 			if (lst->head->next->type == 'I' && lst->head->next->next)
+// 			{
+				
+// 			}
+// 			if (lst->head->next->type == 'O' && lst->head->next->next)
+// 			{
+				
+// 			}
+// 		}
+// 		lst->head = lst->head->next;
+// 	}
+// }
