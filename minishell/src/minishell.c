865c23952dca17cfbb9fe06bc0089cf153c446d9
Anais Gassin <angassin@student.s19.be>
2023-08-14 15:11:28 +0200

    merge ok, commented main.c and buggy code minishell.c

--- minishell/src/minishell.c
+++ minishell/src/minishell.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cchabeau <cchabeau@student.s19.be>         +#+  +:+       +#+        */
+/*   By: angassin <angassin@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/25 17:34:10 by cchabeau          #+#    #+#             */
-/*   Updated: 2023/08/10 11:29:20 by cchabeau         ###   ########.fr       */
+/*   Updated: 2023/08/15 02:34:36 by angassin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,15 @@
 int	main(int argc, char **argv, char **envp)
 {
 	t_tkn_lst	*lst;
-	t_env_lst	*env_lst;
+	// t_env_lst	*env_lst;
 	t_cmd_node	*cmd;
 	char		*res;
 
 	(void)argv;
 	(void)argc;
+	(void)envp;
 	lst = NULL;
-	env_lst = init_envp(envp);
+	// env_lst = init_envp(envp);
 	cmd = init_cmd_node();
 	if (!cmd)
 		return (1);
@@ -34,6 +35,6 @@ int	main(int argc, char **argv, char **envp)
 		// cmd = parsing(lst);
 		// print_cmd(cmd);
 	}
-	clear_env_lst(env_lst->head);
+	// clear_env_lst(env_lst->head);
 	return (0);
 }
