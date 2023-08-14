2aa1ba5711788936810c06d84d0fc22616474540
angassin <AnaisTry@users.noreply.github.com>
2023-08-15 01:16:54 +0200

    removed main.c, corrected add_list_env, renamed exe_utils.c

---
+++ main.c
@@ -0,0 +1,42 @@
+// /* ************************************************************************** */
+// /*                                                                            */
+// /*                                                        :::      ::::::::   */
+// /*   main.c                                             :+:      :+:    :+:   */
+// /*                                                    +:+ +:+         +:+     */
+// /*   By: angassin <angassin@student.42.fr>          +#+  +:+       +#+        */
+// /*                                                +#+#+#+#+#+   +#+           */
+// /*   Created: 2023/07/14 11:10:33 by angassin          #+#    #+#             */
+// /*   Updated: 2023/08/14 13:36:34 by angassin         ###   ########.fr       */
+// /*                                                                            */
+// /* ************************************************************************** */
+
+// #include "../includes/minishell.h"
+
+// int	main(int argc, char **argv, char **envp)
+// {
+// 	int		status;
+// 	t_cmd	cmd;
+
+// 	(void)argc;
+// 	cmd.type_in = STDIN_OUT;
+// 	cmd.type_out = STDIN_OUT;
+// 	cmd.infile = "infile.txt";
+// 	cmd.outfile = "out";
+// 	cmd.cmd = &argv[1];
+// 	cmd.next= NULL;
+
+// 	status = execution(&cmd, envp);
+// 	// char	*cmd_line;
+
+// 	// while (true)
+// 	// {
+// 	// 	status = 0;
+// 	// 	cmd_line = readline("[Minishell] > ");
+// 	// 	if (ft_strlen(cmd_line) > 0)
+// 	// 		add_history(cmd_line);
+// 	// 	printf("%s\n", cmd_line);
+// 	// 	if (ft_strncmp(cmd_line, "cd", 2) == OK)
+// 	// 		status = cd(cmd_line);
+// 	// }
+// 	return (status);
+// }
