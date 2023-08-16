9850f101138cb7d8ecb8651caa3c9872cdadc15c
AnaisTry <angassin@student.s19.be>
2023-08-16 17:30:35 +0200

    commented calvin's part

--- minishell/src/minishell.c
+++ minishell/src/minishell.c
@@ -6,7 +6,7 @@
 /*   By: angassin <angassin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/25 17:34:10 by cchabeau          #+#    #+#             */
-/*   Updated: 2023/08/16 17:13:45 by angassin         ###   ########.fr       */
+/*   Updated: 2023/08/16 17:24:54 by angassin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,19 +17,19 @@ int	main(int argc, char **argv, char **envp)
 	t_tkn_lst	*lst;
 	t_env_lst	*env_lst;
 	t_cmd_dllst	*cmd_table;
-	char	*cmd_line;
+	// char	*cmd_line;
 	int		status;
-	// t_cmd	cmd;
+	t_cmd	cmd;
 
-	// (void)argc;
-	// cmd.type_in = STDIN_OUT;
-	// cmd.type_out = STDIN_OUT;
-	// cmd.infile = "infile.txt";
-	// cmd.outfile = "out";
-	// cmd.cmd = &argv[1];
-	// cmd.next= NULL;
+	(void)argc;
+	cmd.type_in = STDIN_OUT;
+	cmd.type_out = STDIN_OUT;
+	cmd.infile = "infile.txt";
+	cmd.outfile = "out";
+	cmd.cmd = &argv[1];
+	cmd.next= NULL;
 
-	// status = execution(&cmd, envp);	
+	status = execution(&cmd, envp);	
 	(void)argv;
 	(void)argc;
 	(void)envp;
@@ -38,19 +38,19 @@ int	main(int argc, char **argv, char **envp)
 	cmd_table = init_cmd_dllst();
 	if (!cmd_table)
 		return (1);
-	while (true)
-	{
-		status = 0;
-		cmd_line = readline("[Minishell] > ");
-		lst = lexing(cmd_line);
-		if (ft_strlen(cmd_line) > 0)
-			add_history(cmd_line);
-		print_token(lst->head);
-		// cmd_table = parsing(lst);
-		// print_cmd(cmd_table);
-		// if (ft_strncmp(cmd_line, "cd", 2) == OK)
-		// 	status = cd(cmd_line + 3);
-	}
-	clear_env_lst(env_lst->head);
+	// while (true)
+	// {
+	// 	status = 0;
+	// 	cmd_line = readline("[Minishell] > ");
+	// 	lst = lexing(cmd_line);
+	// 	if (ft_strlen(cmd_line) > 0)
+	// 		add_history(cmd_line);
+	// 	print_token(lst->head);
+	// 	// cmd_table = parsing(lst);
+	// 	// print_cmd(cmd_table);
+	// 	// if (ft_strncmp(cmd_line, "cd", 2) == OK)
+	// 	// 	status = cd(cmd_line + 3);
+	// }
+	// clear_env_lst(env_lst->head);
 	return (status);
 }
