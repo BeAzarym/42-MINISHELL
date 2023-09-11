b08cfaf7b7b5a68e46372de0acc4ccd5bcb5e27d
AnaisTry <angassin@student.s19.be>
2023-09-11 15:49:47 +0200

    implemented sigaction more correctly

--- minishell/src/execution/signals.c
+++ minishell/src/execution/signals.c
@@ -6,11 +6,21 @@
 /*   By: angassin <angassin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/20 23:40:50 by angassin          #+#    #+#             */
-/*   Updated: 2023/08/28 13:08:47 by angassin         ###   ########.fr       */
+/*   Updated: 2023/09/11 15:48:41 by angassin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/execute.h"
+
+void	set_signal_handler(int signal, int flags, void (*handler)(int))
+{
+	struct sigaction	sa;
+
+	sa.sa_handler = handler;
+	sa.sa_flags = flags;
+	sigemptyset(&sa.sa_mask);
+	sigaction(signal, &sa, NULL);
+}
 
 /* 
 	Ignore "Ctrl-c"
@@ -18,22 +28,14 @@
 */
 void	ignore_shell_signal(void)
 {
-	struct sigaction	sa;
-
-	sa.sa_handler = SIG_IGN;
-	sa.sa_flags = 0;
-	sigemptyset(&sa.sa_mask);
-	sigaction(SIGINT, &sa, NULL);
-	sigaction(SIGQUIT, &sa, NULL);
+	set_signal_handler(SIGINT, 0, SIG_IGN);
+	set_signal_handler(SIGQUIT, 0, SIG_IGN);
 	g_signalset = true;
 }
 
 // ctrl-c
 void	set_sigint_in_child(int signal)
 {
-	struct sigaction	sa;
-
-	(void)signal;
-	if (g_signalset == true)
-		sigaction(SIGINT, &sa, NULL);
+	if (g_signalset && signal == SIGINT)
+		set_signal_handler(SIGINT, 0, SIG_DFL);
 }
