d3f7ee9a7ef3f9de8501ff98b2c59d25578c1f1d
BeAzarym <calvinchabeau01@gmail.com>
2023-09-25 18:17:39 +0200

    norm

--- minishell/src/builtins/export_utils.c
+++ minishell/src/builtins/export_utils.c
@@ -6,16 +6,16 @@
 /*   By: cchabeau <cchabeau@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/25 17:02:43 by cchabeau          #+#    #+#             */
-/*   Updated: 2023/09/25 17:28:26 by cchabeau         ###   ########.fr       */
+/*   Updated: 2023/09/25 18:17:20 by cchabeau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/builtins.h"
 
-void replace_value_env(char *key, char *value, t_env_lst *env)
+void	replace_value_env(char *key, char *value, t_env_lst *env)
 {
-	t_env *cpy;
-	
+	t_env	*cpy;
+
 	cpy = env->head;
 	while (cpy)
 	{
@@ -32,7 +32,17 @@ void replace_value_env(char *key, char *value, t_env_lst *env)
 // void sort_env(t_env_lst *env)
 // {
 // 	t_env *cpy;
+// 	t_env *tmp;
+// 	int i;
 
 // 	cpy = env->head;
-	
+// 	i = 0;
+// 	while (i < env->size-1)
+// 	{
+// 		if (ft_strcmp(cpy->key, cpy->next->key))
+// 		{
+// 			tmp = cpy;
+
+// 		}
+// 		}
 // }
