aa82e90aa666b78a47df23918de1eb69e1dd2a9d
angassin <AnaisTry@users.noreply.github.com>
2023-10-01 23:01:26 +0200

    added conditions for wait

--- minishell/src/execution/wait.c
+++ minishell/src/execution/wait.c
@@ -6,7 +6,7 @@
 /*   By: angassin <angassin@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/25 19:18:15 by angassin          #+#    #+#             */
-/*   Updated: 2023/09/25 19:22:42 by angassin         ###   ########.fr       */
+/*   Updated: 2023/10/01 22:58:06 by angassin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,15 @@ int	processes_wait(const pid_t pid, int size)
 
 	waitpid(pid, &status, 0);
 	exit_status = WEXITSTATUS(status);
+	if (WIFSIGNALED(status))
+	{
+		if (WTERMSIG(status) == 1)
+			exit_status = 130;
+		else if (WTERMSIG(status) == SIGQUIT)
+			exit_status = 131;
+		else
+			exit_status = 128 + WTERMSIG(status);
+	}
 	i = 1;
 	while (i < size)
 	{
