34b754e6c1834d770a79d9b7c1b2a40ce45a2c8a
AnaisTry <angassin@student.s19.be>
2023-10-06 18:06:58 +0200

    exit handles long, norm ok

--- minishell/src/builtins/exit.c
+++ minishell/src/builtins/exit.c
@@ -6,13 +6,14 @@
 /*   By: angassin <angassin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/24 03:27:11 by angassin          #+#    #+#             */
-/*   Updated: 2023/10/05 16:51:00 by angassin         ###   ########.fr       */
+/*   Updated: 2023/10/06 18:06:16 by angassin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/builtins.h"
 
 static bool	ft_isnumber(char *cmd);
+static int	exit_atoi(char *str);
 
 /*
 	Exits the shell.
@@ -33,13 +34,14 @@ int	exit_builtin(char **cmd)
 		g_status = 1;
 		return (g_status);
 	}
-	else if (cmd[1] != NULL && !ft_isnumber(cmd[1]))
+	else if ((cmd[1] != NULL && !ft_isnumber(cmd[1]))
+		|| (cmd[1] != NULL && exit_atoi(cmd[1]) >= 255))
 	{
 		printf("minishell: exit: %s: numeric argument required\n", cmd[1]);
 		exit (255);
 	}
 	else if (cmd[1] != NULL && ft_isnumber(cmd[1]))
-		exit(ft_atoi(cmd[1]));
+		exit(exit_atoi(cmd[1]));
 	exit(g_status);
 }
 
@@ -62,4 +64,31 @@ static bool	ft_isnumber(char *cmd)
 		++i;
 	}
 	return (sign <= 1);
+}
+
+static int	exit_atoi(char *str)
+{
+	int		i;
+	int		sign;
+	long	nb;
+	long	tmp;
+
+	i = 0;
+	sign = 1;
+	nb = 0;
+	while (str[i] == ' ' || (str[i] >= '\t' && str[i] <= '\r'))
+		i++;
+	if (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i++] == '-')
+			sign *= -1;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		tmp = nb;
+		nb = nb * 10 + str[i++] - '0';
+		if (nb - (sign == -1) < tmp)
+			return (256);
+	}
+	return (nb * sign);
 }
